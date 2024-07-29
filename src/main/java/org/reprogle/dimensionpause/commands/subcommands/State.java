76fff95bc6526e7ba811eba83840de0db080432d
EarthCow <coppergriffing@gmail.com>
2024-07-28 22:56:16 -0400

    Enables running commands via console

--- src/main/java/org/reprogle/dimensionpause/commands/subcommands/State.java
+++ src/main/java/org/reprogle/dimensionpause/commands/subcommands/State.java
@@ -1,6 +1,6 @@
 package org.reprogle.dimensionpause.commands.subcommands;
 
-import org.bukkit.entity.Player;
+import org.bukkit.command.CommandSender;
 import org.reprogle.dimensionpause.commands.CommandFeedback;
 import org.reprogle.dimensionpause.commands.SubCommand;
 
@@ -14,19 +14,19 @@ public class State implements SubCommand {
 	}
 
 	@Override
-	public void perform(Player p, String[] args) {
+	public void perform(CommandSender sender, String[] args) {
 		if (args.length >= 2) {
 			switch (args[1].toLowerCase()) {
-				case "nether", "end" -> p.sendMessage(CommandFeedback.sendCommandFeedback("state", args[1].toLowerCase()));
-				default -> p.sendMessage(CommandFeedback.sendCommandFeedback("usage"));
+				case "nether", "end" -> sender.sendMessage(CommandFeedback.sendCommandFeedback("state", args[1].toLowerCase()));
+				default -> sender.sendMessage(CommandFeedback.sendCommandFeedback("usage"));
 			}
 		} else {
-			p.sendMessage(CommandFeedback.sendCommandFeedback("usage"));
+			sender.sendMessage(CommandFeedback.sendCommandFeedback("usage"));
 		}
 	}
 
 	@Override
-	public List<String> getSubcommands(Player p, String[] args) {
+	public List<String> getSubcommands(CommandSender sender, String[] args) {
 		List<String> subcommands = new ArrayList<>();
 
 		// We are already in argument 1 of the command, hence why this is a subcommand
