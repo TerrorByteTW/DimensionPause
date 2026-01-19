2442eefc7c7c91d59549c70d1b6b4bc7ce6c7eb8
Nate Reprogle <nate.reprogle@gmail.com>
2026-01-18 19:54:53 -0600

    Update build scripts with correct supported versions of Paper

--- .github/workflows/publish-to-hangar.sh
+++ .github/workflows/publish-to-hangar.sh
@@ -60,7 +60,7 @@ cat > versionUpload.json <<EOF
   "channel": "$CHANNEL",
   "description": "$DESCRIPTION",
   "platformDependencies": {
-    "PAPER": ["1.20","1.20.1","1.20.2","1.20.3","1.20.4","1.20.5","1.20.6","1.21","1.21.1","1.21.2","1.21.3","1.21.4","1.21.5","1.21.6","1.21.7","1.21.8"]
+    "PAPER": ["1.21.11"]
   },
   "pluginDependencies": {},
   "files": [
